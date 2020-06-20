16c06872b943810255051f1df336625c98d934b6
Diego Saavedra Villaran <diegosaavedra123@hotmail.com>
2020-06-20 11:58:35 -0500

    caso2

---
+++ Caso2/main2.js
@@ -0,0 +1,89 @@
+function menuGeneral1() { 
+    document.getElementById("ele1").style.visibility=`visible`
+    document.getElementById("ele1").style.display=`block`;
+    
+    document.getElementById("ele2").style.visibility=`hidden`;
+    document.getElementById("ele2").style.display=`none`;
+
+    document.getElementById("ele3").style.visibility=`hidden`;
+    document.getElementById("ele3").style.display=`none`;
+    
+    document.getElementById("ele4").style.visibility=`hidden`;
+    document.getElementById("ele4").style.display=`none`;
+    
+    document.getElementById("ele5").style.visibility=`hidden`;
+    document.getElementById("ele5").style.display=`none`;
+
+}
+
+function menuGeneral2() {
+    document.getElementById("ele2").style.visibility=`visible`
+    document.getElementById("ele2").style.display=`block`;
+   
+    document.getElementById("ele1").style.visibility=`hidden`;
+    document.getElementById("ele1").style.display=`none`;
+
+    document.getElementById("ele3").style.visibility=`hidden`;
+    document.getElementById("ele3").style.display=`none`;
+    
+    document.getElementById("ele4").style.visibility=`hidden`;
+    document.getElementById("ele4").style.display=`none`;
+    
+    document.getElementById("ele5").style.visibility=`hidden`;
+    document.getElementById("ele5").style.display=`none`;
+}
+
+function menuGeneral3() {
+    document.getElementById("ele3").style.visibility=`visible`
+    document.getElementById("ele3").style.display=`block`;
+
+
+    document.getElementById("ele1").style.visibility=`hidden`;
+    document.getElementById("ele1").style.display=`none`;
+    
+    document.getElementById("ele2").style.visibility=`hidden`;
+    document.getElementById("ele2").style.display=`none`;
+    
+    document.getElementById("ele4").style.visibility=`hidden`;
+    document.getElementById("ele4").style.display=`none`;
+    
+    document.getElementById("ele5").style.visibility=`hidden`;
+    document.getElementById("ele5").style.display=`none`;
+}
+
+function menuGeneral4() {
+    document.getElementById("ele4").style.visibility=`visible`
+    document.getElementById("ele4").style.display=`block`;
+
+    document.getElementById("ele1").style.visibility=`hidden`;
+    document.getElementById("ele1").style.display=`none`;
+    
+    document.getElementById("ele2").style.visibility=`hidden`;
+    document.getElementById("ele2").style.display=`none`;
+
+
+    document.getElementById("ele3").style.visibility=`hidden`;
+    document.getElementById("ele3").style.display=`none`;
+
+
+    document.getElementById("ele5").style.visibility=`hidden`;
+    document.getElementById("ele5").style.display=`none`;
+}
+
+function menuGeneral5() {
+    document.getElementById("ele5").style.visibility=`visible`
+    document.getElementById("ele5").style.display=`block`;
+
+    document.getElementById("ele1").style.visibility=`hidden`;
+    document.getElementById("ele1").style.display=`none`;
+    
+    document.getElementById("ele2").style.visibility=`hidden`;
+    document.getElementById("ele2").style.display=`none`;
+
+
+    document.getElementById("ele3").style.visibility=`hidden`;
+    document.getElementById("ele3").style.display=`none`;
+
+    document.getElementById("ele4").style.visibility=`hidden`;
+    document.getElementById("ele4").style.display=`none`;
+}
